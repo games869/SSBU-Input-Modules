c4e71ff86b306f2f9f030f0310e81593ffebf1c6
games869 <112382902+games869@users.noreply.github.com>
2026-03-05 00:58:18 -0500

    debug time

--- src/ChargeInputModule.rs
+++ src/ChargeInputModule.rs
@@ -412,9 +412,10 @@ pub unsafe fn update_module(module_accessor:*mut BattleObjectModuleAccessor, fra
     let entry_id = WorkModule::get_int(module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
     let last_update_frame = &mut charge_input_storage[entry_id].2;
     let prev_last_frame = *last_update_frame;
+
+    println!("frame: {frame}, ignore_repeat_frame: {ignore_repeat_frame}, update_charge_time: {update_charge_time}");
     
     if frame == *last_update_frame && !ignore_repeat_frame {
-
 
         return;
 
@@ -454,6 +455,7 @@ pub unsafe fn update_module(module_accessor:*mut BattleObjectModuleAccessor, fra
                     if per_input[input].charge_time < per_dir[input][step].required_charge_time {
             
                         per_input[input].charge_time += 1;
+                        println!("new charge: {}", per_input[input].charge_time);
 
                     }
                 }
